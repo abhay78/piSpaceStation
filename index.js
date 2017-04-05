26738c0ef1db786633fa6bc087e934feb51337f2
Abhay Rajan <arajan@tycoint.com>
2017-04-05 07:23:46 -0500

    track iss from my location

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const got = require('got');
+const geolib = require('geolib');
 const url = "http://api.open-notify.org/iss-now.json";
+const myPosition = { latitude: 30.484429, longitude: -97.788762 };
+
 let delaySeconds = 3000;
 
 function loop(){
   got(url, { json: true})
     .then(iss => {
-      console.log(iss.body.iss_position);
+      const iss_position = iss.body.iss_position;
+      const distanceFromISS = geolib.getDistance(myPosition, iss_position);
+      const distanceFromISSInMiles = geolib.convertUnit('mi', distanceFromISS, 2);
+      console.log(`my distance from ISS ${distanceFromISSInMiles} miles`);
     })
     .catch(error => {
       console.log(error.response.body);
